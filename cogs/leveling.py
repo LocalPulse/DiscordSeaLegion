2b7afc0fa1f51a8a809a482744fd8ff9da3019a8
vitalimichalov <yt4funset@gmail.com>
2024-11-13 18:44:20 +0300

    update

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -157,7 +157,6 @@ class Leveling(commands.Cog):
                 f"{inter.author.mention}, у вас пока нет XP. Начните писать сообщения, чтобы зарабатывать XP!")
 
     @commands.slash_command(description="Показывает топ-10 пользователей по опыту")
-    @commands.command(name="leaderboard")
     async def leaderboard(self, inter: disnake.ApplicationCommandInteraction):
 
         if not user_data:
