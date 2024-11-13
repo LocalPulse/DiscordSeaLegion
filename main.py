055f5b9bbe0d3b6e61ddad36a85f50e244cb84b9
vitalimichalov <yt4funset@gmail.com>
2024-11-13 18:10:08 +0300

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = disnake.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
+bot = commands.Bot(command_prefix="sl.", intents=intents, help_command=None)
 
 bot.load_extension("cogs.leveling")
 bot.load_extension("cogs.role_management")
